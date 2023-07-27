a18e81132f86b4abb8410e02d1e44fe6dc07a095
Patrik <trikteixeira@gmail.com>
2023-07-27 09:23:05 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -43,6 +43,8 @@ public class Order implements Serializable {
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
 	
+	
+	
 	public Order() {
 	}
 
@@ -98,6 +100,14 @@ public class Order implements Serializable {
 
 	public Set<OrderItem> getItems() {
 		return items;
+	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum =  sum + x.getSubTotal();
+		}
+		return sum;
 	}
 
 	@Override
